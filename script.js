d541b9b0f53cea495c99847e54320256368e1dc1
Akash Raghuram R L <221201004@rajalakshmi.edu.in>
2025-08-07 23:05:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let model;
+let modelLoaded = false;
 
 async function loadModel() {
   model = await tf.loadLayersModel('model/model.json');
   console.log("✅ Model loaded");
+  modelLoaded = true;
 }
 
 loadModel();
 
 document.getElementById('imageUpload').addEventListener('change', async function (event) {
+  if (!modelLoaded) {
+    alert("Model not loaded yet. Please wait.");
+    return;
+  }
+
   const file = event.target.files[0];
   const img = document.getElementById('preview');
   img.src = URL.createObjectURL(file);
+
   img.onload = async () => {
     const tensor = tf.browser
       .fromPixels(img)
@@ -33,5 +41,3 @@ document.getElementById('imageUpload').addEventListener('change', async function
       `Prediction: ${classLabels[predictedClass]}`;
   };
 });
-
-
